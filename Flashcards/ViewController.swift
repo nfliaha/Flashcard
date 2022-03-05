50e9ef9968aaf12bc842a064768a56ae24faab78
nfliaha <nfliaha1@gmail.com>
2022-03-05 00:39:48 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Nadzeya Fliaha on 3/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
